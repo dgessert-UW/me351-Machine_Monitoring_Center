96b61e39e5b8ab3ae0d8a656dd5f295269552f77
dgessert-UW <99203836+dgessert-UW@users.noreply.github.com>
2022-11-01 21:08:08 -0500

    Update MMC_Modbus.py

--- MMC_Modbus.py
+++ MMC_Modbus.py
@@ -12,7 +12,18 @@ def modbus_connect(parity = 'E',bytesize =8,baudrate =19200,stopbits = 1,port='/
         return False
 
 def modbus_read_holding_registers(client,address, count, slave_id):
-    read=client.read_holding_registers(address = address,count =count, unit = slave_id)#address = 100 ,count =2,unit=sensor) 
+    read=client.read_holding_registers(address = address,count =count, unit = slave_id) 
+
+    data = read.registers
+    
+    a = data[0]
+    b = data[1]
+    reading = str(struct.unpack('>f', bytes.fromhex(f"{a:0>4x}" + f"{b:0>4x}"))[0])
+    reading = str(round(float(reading),3))
+    return reading
+
+def modbus_read_holding_registers(client,address, count, slave_id):
+    read=client.write_register(address = address,count =count, unit = slave_id) 
 
     data = read.registers
     
