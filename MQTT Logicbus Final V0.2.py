a467b2342ba32f4f437703c1ed3aba7ce2f56fdf
dylangessert <99202666+dylangessert@users.noreply.github.com>
2022-11-08 11:17:43 -0600

    Update MQTT Logicbus Final V0.2.py

--- MQTT Logicbus Final V0.2.py
+++ MQTT Logicbus Final V0.2.py
@@ -41,20 +41,20 @@ while True:
                 
                 payload =str({'Time':datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),'Output':str(modbus_return),'Unit':'C','Sensor_ID_Code':str(existing_devices[device_name]['Type'])+' '+str(existing_devices[device_name]['SlaveID'])})
             except:
-                try:        
-                    modbus_connection = MMC_Modbus.modbus_connect()
-                    if modbus_connection == False:
-                        payload =str({'Time':datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),'Output':'Unable Connect to Converter','Unit':'-','Sensor_ID_Code':str(existing_devices[device_name]['Type'])+' '+str(existing_devices[device_name]['SlaveID'])})
-                    else:
-                        try:
-                            modbus_return = MMC_Modbus.modbus_read_holding_registers(modbus_connection ,existing_devices[device_name]['Address'],
-                                                                            existing_devices[device_name]['Count'],
-                                                                            existing_devices[device_name]['SlaveID'])
-                            
-                            payload =str({'Time':datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),'Output':str(modbus_return),'Unit':'C','Sensor_ID_Code':str(existing_devices[device_name]['Type'])+' '+str(existing_devices[device_name]['SlaveID'])})
-                        except:
-                            payload =str({'Time':datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),'Output':'Device Unresponsive','Unit':'-','Sensor_ID_Code':str(existing_devices[device_name]['Type'])+' '+str(existing_devices[device_name]['SlaveID'])})
-            
+    
+                modbus_connection = MMC_Modbus.modbus_connect()
+                if modbus_connection == False:
+                    payload =str({'Time':datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),'Output':'Unable Connect to Converter','Unit':'-','Sensor_ID_Code':str(existing_devices[device_name]['Type'])+' '+str(existing_devices[device_name]['SlaveID'])})
+                else:
+                    try:
+                        modbus_return = MMC_Modbus.modbus_read_holding_registers(modbus_connection ,existing_devices[device_name]['Address'],
+                                                                        existing_devices[device_name]['Count'],
+                                                                        existing_devices[device_name]['SlaveID'])
+                        
+                        payload =str({'Time':datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),'Output':str(modbus_return),'Unit':'C','Sensor_ID_Code':str(existing_devices[device_name]['Type'])+' '+str(existing_devices[device_name]['SlaveID'])})
+                    except:
+                        payload =str({'Time':datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),'Output':'Device Unresponsive','Unit':'-','Sensor_ID_Code':str(existing_devices[device_name]['Type'])+' '+str(existing_devices[device_name]['SlaveID'])})
+        
 
             try:
                 if len(Offline_Data_Collection) == 0:
